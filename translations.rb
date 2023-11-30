55fc18226240743b7d075b4f88a31dd83dab36c4
azx5g <mail@jbste.cc>
2023-11-30 19:50:53 +0100

    Separated translations.rb in another file

---
+++ translations.rb
@@ -0,0 +1,596 @@
+
+
+  def country_to_language = {
+  # English
+  'AG' => 'en', 'AI' => 'en', 'AQ' => 'en', 'AS' => 'en', 'AU' => 'en',
+  'BB' => 'en', 'BD' => 'en', 'BM' => 'en', 'BS' => 'en', 'BW' => 'en',
+  'BZ' => 'en', 'CA' => 'en', 'CC' => 'en', 'CK' => 'en', 'CM' => 'en',
+  'CX' => 'en', 'CY' => 'en', 'DM' => 'en', 'ER' => 'en', 'FJ' => 'en',
+  'FK' => 'en', 'FM' => 'en', 'GB' => 'en', 'GD' => 'en', 'GG' => 'en',
+  'GH' => 'en', 'GI' => 'en', 'GM' => 'en', 'GU' => 'en', 'GY' => 'en',
+  'HK' => 'en', 'IE' => 'en', 'IM' => 'en', 'IN' => 'en', 'IO' => 'en',
+  'JE' => 'en', 'JM' => 'en', 'KE' => 'en', 'KI' => 'en', 'KN' => 'en',
+  'KY' => 'en', 'LC' => 'en', 'LR' => 'en', 'LS' => 'en', 'MH' => 'en',
+  'MP' => 'en', 'MS' => 'en', 'MT' => 'en', 'MU' => 'en', 'MV' => 'en',
+  'MW' => 'en', 'MY' => 'en', 'NA' => 'en', 'NF' => 'en', 'NG' => 'en',
+  'NR' => 'en', 'NU' => 'en', 'NZ' => 'en', 'PG' => 'en', 'PH' => 'en',
+  'PK' => 'en', 'PN' => 'en', 'PR' => 'en', 'PW' => 'en', 'RW' => 'en',
+  'SB' => 'en', 'SC' => 'en', 'SG' => 'en', 'SH' => 'en',
+  'SL' => 'en', 'SS' => 'en', 'SZ' => 'en', 'TC' => 'en',
+  'TK' => 'en', 'TO' => 'en', 'TT' => 'en', 'TV' => 'en', 'TZ' => 'en',
+  'UG' => 'en', 'UM' => 'en', 'US' => 'en', 'VC' => 'en', 'VG' => 'en',
+  'VI' => 'en', 'VU' => 'en', 'WS' => 'en', 'ZA' => 'en', 'ZM' => 'en',
+  'ZW' => 'en',
+
+  # French
+  'FR' => 'fr', 'MC' => 'fr', 'BJ' => 'fr', 'BF' => 'fr', 'CD' => 'fr', 'CG' => 'fr', 'CI' => 'fr',
+  'GA' => 'fr', 'GN' => 'fr', 'ML' => 'fr', 'NE' => 'fr', 'SN' => 'fr', 'TG' => 'fr', 'BE' => 'fr',
+  'LU' => 'fr', 'CH' => 'fr', 'BI' => 'fr', 'KM' => 'fr', 'DJ' => 'fr',
+  'MG' => 'fr', 'CF' => 'fr', 'TD' => 'fr', 'HT' => 'fr',
+
+  # Spanish
+  'AR' => 'es', 'BO' => 'es', 'CL' => 'es', 'CO' => 'es', 'CR' => 'es', 'CU' => 'es', 'EC' => 'es',
+  'ES' => 'es', 'GT' => 'es', 'GQ' => 'es', 'HN' => 'es', 'MX' => 'es', 'NI' => 'es', 'PA' => 'es',
+  'PY' => 'es', 'PE' => 'es', 'DO' => 'es', 'SV' => 'es', 'UY' => 'es', 'VE' => 'es',
+
+  # Russian
+  'RU' => 'ru', 'BY' => 'ru', 'KZ' => 'ru', 'KG' => 'ru',
+
+  # Arabic
+  'SA' => 'ar', 'BH' => 'ar', 'AE' => 'ar', 'IQ' => 'ar', 'JO' => 'ar', 'KW' => 'ar', 'LB' => 'ar',
+  'OM' => 'ar', 'PS' => 'ar', 'QA' => 'ar', 'YE' => 'ar', 'DZ' => 'ar', 'EG' => 'ar', 'LY' => 'ar',
+  'MA' => 'ar', 'MR' => 'ar', 'SD' => 'ar', 'TN' => 'ar',
+
+  # Chinese
+  'CN' => 'zh', 'TW' => 'zh',
+
+  # Portuguese
+  'PT' => 'pt', 'BR' => 'pt', 'AO' => 'pt', 'CV' => 'pt', 'GW' => 'pt', 'MZ' => 'pt', 'ST' => 'pt',
+  'TL' => 'pt',
+
+  # German
+  'DE' => 'de', 'AT' => 'de', 'LI' => 'de',
+
+  # Japanese
+  'JP' => 'ja',
+
+  # Korean
+  'KR' => 'ko',
+
+  # Italian
+  'IT' => 'it', 'SM' => 'it', 'VA' => 'it',
+
+  # Dutch
+  'NL' => 'nl', 'AW' => 'nl', 'BQ' => 'nl', 'CW' => 'nl', 'SX' => 'nl',
+
+  # Turkish
+  'TR' => 'tr',
+
+  # Polish
+  'PL' => 'pl',
+
+  # Swedish
+  'SE' => 'sv',
+
+  # Norwegian
+  'NO' => 'no',
+
+  # Danish
+  'DK' => 'da',
+
+  # Finnish
+  'FI' => 'fi',
+
+  # Greek
+  'GR' => 'el',
+
+  # Czech
+  'CZ' => 'cs',
+
+  # Hungarian
+  'HU' => 'hu',
+
+  # Romanian
+  'RO' => 'ro',
+
+  # Vietnamese
+  'VN' => 'vi',
+
+  # Thai
+  'TH' => 'th',
+
+  # Indonesian
+  'ID' => 'id',
+
+  # Ukrainian
+  'UA' => 'uk',
+
+  # Croatian
+  'HR' => 'hr',
+
+  # Bulgarian
+  'BG' => 'bg',
+
+  # Slovak
+  'SK' => 'sk',
+
+  # Lithuanian
+  'LT' => 'lt',
+
+  # Slovenian
+  'SI' => 'sl',
+
+  # Latvian
+  'LV' => 'lv',
+
+  # Estonian
+  'EE' => 'et',
+
+  # Serbian
+  'RS' => 'sr',
+
+  # Bosnian
+  'BA' => 'bs',
+
+  # Albanian
+  'AL' => 'sq',
+
+  # Macedonian
+  'MK' => 'mk',
+
+  # Amharic
+  'ET' => 'am',
+
+  # Armenian
+  'AM' => 'hy',
+
+  # Azerbaijani
+  'AZ' => 'az',
+
+
+
+}
+
+  def translations = {
+  'en' => {
+    'ipv4_address' => 'IPv4 Address',
+    'ipv6_address' => 'IPv6 Address',
+    'isp' => 'ISP',
+    'country' => 'Country',
+    'region' => 'Region',
+    'city' => 'City',
+    'os' => 'Operating System',
+    'version' => 'OS Version',
+    'local_ip' => 'Local IP',
+  },
+  'fr' => {
+    'ipv4_address' => 'Adresse IPv4',
+    'ipv6_address' => 'Adresse IPv6',
+    'isp' => 'FAI',
+    'country' => 'Pays',
+    'region' => 'Région',
+    'city' => 'Ville',
+    'os' => 'Système d’exploitation',
+    'version' => 'Version OS',
+    'local_ip' => 'IP Locale',
+  },
+  'es' => {
+    'ipv4_address' => 'Dirección IPv4',
+    'ipv6_address' => 'Dirección IPv6',
+    'isp' => 'Proveedor de Servicios de Internet',
+    'country' => 'País',
+    'region' => 'Región',
+    'city' => 'Ciudad',
+    'os' => 'Sistema Operativo',
+    'version' => 'Versión del SO',
+    'local_ip' => 'IP Local',
+  },
+  'ru' => {
+    'ipv4_address' => 'IPv4-адрес',
+    'ipv6_address' => 'IPv6-адрес',
+    'isp' => 'Интернет-провайдер',
+    'country' => 'Страна',
+    'region' => 'Регион',
+    'city' => 'Город',
+    'os' => 'Операционная Система',
+    'version' => 'Версия ОС',
+    'local_ip' => 'Локальный IP',
+  },
+  'ar' => {
+    'ipv4_address' => 'عنوان آي بي في 4',
+    'ipv6_address' => 'عنوان آي بي في 6',
+    'isp' => 'مزود الخدمة',
+    'country' => 'البلد',
+    'region' => 'المنطقة',
+    'city' => 'المدينة',
+    'os' => 'نظام التشغيل',
+    'version' => 'إصدار النظام',
+    'local_ip' => 'الآي بي المحلي',
+
+  },
+  'zh' => {
+    'ipv4_address' => 'IPv4地址',
+    'ipv6_address' => 'IPv6地址',
+    'isp' => '互联网服务提供商',
+    'country' => '国家',
+    'region' => '地区',
+    'city' => '城市',
+    'os' => '操作系统',
+    'version' => '操作系统版本',
+    'local_ip' => '本地IP',
+  },
+  'pt' => {
+    'ipv4_address' => 'Endereço IPv4',
+    'ipv6_address' => 'Endereço IPv6',
+    'isp' => 'Provedor de Serviços de Internet',
+    'country' => 'País',
+    'region' => 'Região',
+    'city' => 'Cidade',
+    'os' => 'Sistema Operacional',
+    'version' => 'Versão do SO',
+  },
+  'de' => {
+    'ipv4_address' => 'IPv4-Adresse',
+    'ipv6_address' => 'IPv6-Adresse',
+    'isp' => 'Internetdienstanbieter',
+    'country' => 'Land',
+    'region' => 'Region',
+    'city' => 'Stadt',
+    'os' => 'Betriebssystem',
+    'version' => 'OS-Version',
+    'local_ip' => 'Lokale IP',
+  },
+  'ja' => {
+    'ipv4_address' => 'IPv4アドレス',
+    'ipv6_address' => 'IPv6アドレス',
+    'isp' => 'インターネットサービスプロバイダー',
+    'country' => '国',
+    'region' => '地域',
+    'city' => '市',
+    'os' => 'オペレーティングシステム',
+    'version' => 'OSバージョン',
+    'local_ip' => 'ローカルIP',
+  },
+  'ko' => {
+    'ipv4_address' => 'IPv4 주소',
+    'ipv6_address' => 'IPv6 주소',
+    'isp' => '인터넷 서비스 제공자',
+    'country' => '국가',
+    'region' => '지역',
+    'city' => '도시',
+    'os' => '운영 체제',
+    'version' => 'OS 버전',
+    'local_ip' => '로컬 IP',
+  },
+  'it' => {
+    'ipv4_address' => 'Indirizzo IPv4',
+    'ipv6_address' => 'Indirizzo IPv6',
+    'isp' => 'Provider di servizi Internet',
+    'country' => 'Paese',
+    'region' => 'Regione',
+    'city' => 'Città',
+    'os' => 'Sistema operativo',
+    'version' => 'Versione OS',
+    'local_ip' => 'IP locale',
+  },
+  'nl' => {
+    'ipv4_address' => 'IPv4-adres',
+    'ipv6_address' => 'IPv6-adres',
+    'isp' => 'Internetprovider',
+    'country' => 'Land',
+    'region' => 'Regio',
+    'city' => 'Stad',
+    'os' => 'Besturingssysteem',
+    'version' => 'OS-versie',
+    'local_ip' => 'Lokaal IP',
+  },
+  'tr' => {
+    'ipv4_address' => 'IPv4 Adresi',
+    'ipv6_address' => 'IPv6 Adresi',
+    'isp' => 'İnternet Servis Sağlayıcısı',
+    'country' => 'Ülke',
+    'region' => 'Bölge',
+    'city' => 'Şehir',
+    'os' => 'İşletim Sistemi',
+    'version' => 'OS Sürümü',
+    'local_ip' => 'Yerel IP',
+  },
+  'pl' => {
+    'ipv4_address' => 'Adres IPv4',
+    'ipv6_address' => 'Adres IPv6',
+    'isp' => 'Dostawca usług internetowych',
+    'country' => 'Kraj',
+    'region' => 'Region',
+    'city' => 'Miasto',
+    'os' => 'System operacyjny',
+    'version' => 'Wersja systemu operacyjnego',
+    'local_ip' => 'Lokalny IP',
+  },
+  'sv' => {
+    'ipv4_address' => 'IPv4-adress',
+    'ipv6_address' => 'IPv6-adress',
+    'isp' => 'Internetleverantör',
+    'country' => 'Land',
+    'region' => 'Region',
+    'city' => 'Stad',
+    'os' => 'Operativsystem',
+    'version' => 'OS-version',
+    'local_ip' => 'Lokal IP',
+  },
+  'no' => {
+    'ipv4_address' => 'IPv4-adresse',
+    'ipv6_address' => 'IPv6-adresse',
+    'isp' => 'Internettleverandør',
+    'country' => 'Land',
+    'region' => 'Region',
+    'city' => 'By',
+    'os' => 'Operativsystem',
+    'version' => 'OS-versjon',
+    'local_ip' => 'Lokal IP',
+  },
+  'da' => {
+    'ipv4_address' => 'IPv4-adresse',
+    'ipv6_address' => 'IPv6-adresse',
+    'isp' => 'Internetudbyder',
+    'country' => 'Land',
+    'region' => 'Region',
+    'city' => 'By',
+    'os' => 'Operativsystem',
+    'version' => 'OS-version',
+    'local_ip' => 'Lokal IP',
+  },
+  'fi' => {
+    'ipv4_address' => 'IPv4-osoite',
+    'ipv6_address' => 'IPv6-osoite',
+    'isp' => 'Internet-palveluntarjoaja',
+    'country' => 'Maa',
+    'region' => 'Alue',
+    'city' => 'Kaupunki',
+    'os' => 'Käyttöjärjestelmä',
+    'version' => 'Käyttöjärjestelmän versio',
+    'local_ip' => 'Paikallinen IP',
+  },
+  'el' => {
+    'ipv4_address' => 'Διεύθυνση IPv4',
+    'ipv6_address' => 'Διεύθυνση IPv6',
+    'isp' => 'Πάροχος υπηρεσιών Internet',
+    'country' => 'Χώρα',
+    'region' => 'Περιοχή',
+    'city' => 'Πόλη',
+    'os' => 'Λειτουργικό σύστημα',
+    'version' => 'Έκδοση OS',
+    'local_ip' => 'Τοπική IP',
+  },
+  'cs' => {
+    'ipv4_address' => 'IPv4 Adresa',
+    'ipv6_address' => 'IPv6 Adresa',
+    'isp' => 'Poskytovatel internetových služeb',
+    'country' => 'Země',
+    'region' => 'Region',
+    'city' => 'Město',
+    'os' => 'Operační systém',
+    'version' => 'Verze OS',
+    'local_ip' => 'Místní IP',
+  },
+  'hu' => {
+    'ipv4_address' => 'IPv4 cím',
+    'ipv6_address' => 'IPv6 cím',
+    'isp' => 'Internet-szolgáltató',
+    'country' => 'Ország',
+    'region' => 'Régió',
+    'city' => 'Város',
+    'os' => 'Operációs rendszer',
+    'version' => 'OS verzió',
+    'local_ip' => 'Helyi IP',
+  },
+  'ro' => {
+    'ipv4_address' => 'Adresă IPv4',
+    'ipv6_address' => 'Adresă IPv6',
+    'isp' => 'Furnizor de servicii Internet',
+    'country' => 'Țară',
+    'region' => 'Regiune',
+    'city' => 'Oraș',
+    'os' => 'Sistem de operare',
+    'version' => 'Versiunea OS',
+    'local_ip' => 'IP local',
+  },
+  'vi' => {
+    'ipv4_address' => 'Địa chỉ IPv4',
+    'ipv6_address' => 'Địa chỉ IPv6',
+    'isp' => 'Nhà cung cấp dịch vụ Internet',
+    'country' => 'Quốc gia',
+    'region' => 'Vùng',
+    'city' => 'Thành phố',
+    'os' => 'Hệ điều hành',
+    'version' => 'Phiên bản OS',
+    'local_ip' => 'IP cục bộ',
+  },
+  'th' => {
+    'ipv4_address' => 'ที่อยู่ IPv4',
+    'ipv6_address' => 'ที่อยู่ IPv6',
+    'isp' => 'ผู้ให้บริการอินเทอร์เน็ต',
+    'country' => 'ประเทศ',
+    'region' => 'ภูมิภาค',
+    'city' => 'เมือง',
+    'os' => 'ระบบปฏิบัติการ',
+    'version' => 'เวอร์ชัน OS',
+    'local_ip' => 'IP ท้องถิ่น',
+  },
+  'id' => {
+    'ipv4_address' => 'Alamat IPv4',
+    'ipv6_address' => 'Alamat IPv6',
+    'isp' => 'Penyedia Layanan Internet',
+    'country' => 'Negara',
+    'region' => 'Wilayah',
+    'city' => 'Kota',
+    'os' => 'Sistem Operasi',
+    'version' => 'Versi OS',
+    'local_ip' => 'IP Lokal',
+  },
+  'uk' => {
+    'ipv4_address' => 'IPv4-адреса',
+    'ipv6_address' => 'IPv6-адреса',
+    'isp' => 'Інтернет-провайдер',
+    'country' => 'Країна',
+    'region' => 'Регіон',
+    'city' => 'Місто',
+    'os' => 'Операційна система',
+    'version' => 'Версія ОС',
+    'local_ip' => 'Локальний IP',
+  },
+  'hr' => {
+    'ipv4_address' => 'IPv4 adresa',
+    'ipv6_address' => 'IPv6 adresa',
+    'isp' => 'Internetski pružatelj usluga',
+    'country' => 'Zemlja',
+    'region' => 'Regija',
+    'city' => 'Grad',
+    'os' => 'Operativni sustav',
+    'version' => 'Verzija OS-a',
+    'local_ip' => 'Lokalna IP adresa',
+  },
+  'bg' => {
+    'ipv4_address' => 'IPv4 адрес',
+    'ipv6_address' => 'IPv6 адрес',
+    'isp' => 'Интернет доставчик',
+    'country' => 'Държава',
+    'region' => 'Регион',
+    'city' => 'Град',
+    'os' => 'Операционна система',
+    'version' => 'Версия на ОС',
+    'local_ip' => 'Локален IP',
+  },
+  'sk' => {
+    'ipv4_address' => 'IPv4 adresa',
+    'ipv6_address' => 'IPv6 adresa',
+    'isp' => 'Poskytovateľ internetových služieb',
+    'country' => 'Krajina',
+    'region' => 'Región',
+    'city' => 'Mesto',
+    'os' => 'Operačný systém',
+    'version' => 'Verzia OS',
+    'local_ip' => 'Lokálna IP',
+  },
+  'lt' => {
+    'ipv4_address' => 'IPv4 adresas',
+    'ipv6_address' => 'IPv6 adresas',
+    'isp' => 'Internetinės paslaugos teikėjas',
+    'country' => 'Šalis',
+    'region' => 'Regionas',
+    'city' => 'Miestas',
+    'os' => 'Operacinė sistema',
+    'version' => 'OS versija',
+    'local_ip' => 'Vietinis IP',
+  },
+  'sl' => {
+    'ipv4_address' => 'IPv4 naslov',
+    'ipv6_address' => 'IPv6 naslov',
+    'isp' => 'Ponudnik internetnih storitev',
+    'country' => 'Država',
+    'region' => 'Regija',
+    'city' => 'Mesto',
+    'os' => 'Operacijski sistem',
+    'version' => 'Različica OS',
+    'local_ip' => 'Lokalni IP',
+  },
+  'lv' => {
+    'ipv4_address' => 'IPv4 adrese',
+    'ipv6_address' => 'IPv6 adrese',
+    'isp' => 'Interneta pakalpo sniedzējs',
+    'country' => 'Valsts',
+    'region' => 'Reģions',
+    'city' => 'Pilsēta',
+    'os' => 'Operētājsistēma',
+    'version' => 'OS versija',
+    'local_ip' => 'Lokālais IP',
+  },
+  'et' => {
+    'ipv4_address' => 'IPv4 aadress',
+    'ipv6_address' => 'IPv6 aadress',
+    'isp' => 'Internetiteenuse pakkuja',
+    'country' => 'Riik',
+    'region' => 'Regioon',
+    'city' => 'Linn',
+    'os' => 'Operatsioonisüsteem',
+    'version' => 'OS-i versioon',
+    'local_ip' => 'Kohalik IP',
+  },
+  'am' => {
+    'ipv4_address' => 'IPv4 հասցե',
+    'ipv6_address' => 'IPv6 հասցե',
+    'isp' => 'Ինտերնետային ծառայության մասնագետ',
+    'country' => 'Երկիր',
+    'region' => 'Տարածաշրջան',
+    'city' => 'Քաղաք',
+    'os' => 'Համակարգչային համակարգ',
+    'version' => 'OS տարբերակ',
+    'local_ip' => 'Տեղական IP',
+  },
+  'hy' => {
+    'ipv4_address' => 'IPv4 հասցե',
+    'ipv6_address' => 'IPv6 հասցե',
+    'isp' => 'Ինտերնետային ծառայության մասնագետ',
+    'country' => 'Երկիր',
+    'region' => 'Տարածաշրջան',
+    'city' => 'Քաղաք',
+    'os' => 'Համակարգչային համակարգ',
+    'version' => 'OS տարբերակ',
+    'local_ip' => 'Տեղական IP',
+  },
+  'az' => {
+    'ipv4_address' => 'IPv4 ünvanı',
+    'ipv6_address' => 'IPv6 ünvanı',
+    'isp' => 'İnternet xidmətləri provayderi',
+    'country' => 'Ölkə',
+    'region' => 'Region',
+    'city' => 'Şəhər',
+    'os' => 'Əməliyyat sistemi',
+    'version' => 'OS versiyası',
+    'local_ip' => 'Yerli IP',
+  },
+  'bs' => {
+    'ipv4_address' => 'IPv4 adresa',
+    'ipv6_address' => 'IPv6 adresa',
+    'isp' => 'Internet provajder',
+    'country' => 'Zemlja',
+    'region' => 'Regija',
+    'city' => 'Grad',
+    'os' => 'Operativni sistem',
+    'version' => 'Verzija OS',
+    'local_ip' => 'Lokalna IP adresa',
+  },
+  'sq' => {
+    'ipv4_address' => 'Adresa IPv4',
+    'ipv6_address' => 'Adresa IPv6',
+    'isp' => 'Furnizuesi i shërbimeve të internetit',
+    'country' => 'Shtet',
+    'region' => 'Rajon',
+    'city' => 'Qytet',
+    'os' => 'Sistemi operativ',
+    'version' => 'Versioni OS',
+    'local_ip' => 'IP lokal',
+  },
+  'mk' => {
+    'ipv4_address' => 'IPv4 адреса',
+    'ipv6_address' => 'IPv6 адреса',
+    'isp' => 'Интернет провајдер',
+    'country' => 'Земја',
+    'region' => 'Регион',
+    'city' => 'Град',
+    'os' => 'Оперативен систем',
+    'version' => 'Верзија на OS',
+    'local_ip' => 'Локална IP адреса',
+  },
+  'sr' => {
+    'ipv4_address' => 'IPv4 адреса',
+    'ipv6_address' => 'IPv6 адреса',
+    'isp' => 'Интернет провајдер',
+    'country' => 'Земља',
+    'region' => 'Регион',
+    'city' => 'Град',
+    'os' => 'Оперативни систем',
+    'version' => 'Верзија OS',
+    'local_ip' => 'Локална IP адреса',
+  },
+}
+
